da70f5833c851f60f6bca33be162297e4a15a968
nersus15 <fathur.ashter15@gmail>
2019-08-12 21:44:45 +0800

    Initial Project

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+// Setting Listenig Server
+app.listen(3001);
